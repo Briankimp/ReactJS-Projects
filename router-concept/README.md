b878a7d0355d8f4d85c523238d3e4c4a61ee01d1
Brian <bkimathi973@gmail.com>
2024-06-06 16:17:48 +0300

    Demo Project for React Concepts

---
+++ router-concept/README.md
@@ -0,0 +1,18 @@
+# Website to demonstrate React Router
+
+This is a Simple webiste to display the use of the react routers.
+
+## Lessons Learnt
+
+React Routing is one of the core concepts of react.
+I have learnt the logic of how react-router-dom library is used to handle the routing, I am now able to use most of the components in the library to manipulate the DOM.
+I have learnt to use outlet to render child elements in a route congfig. For example in the code; The layout component has the outlet to render  all the other components in the DOM
+
+ Links. These are used to create navigational links to routes in react app.
+ I have learnt the use of useParam hook to access URL parameters andextract dynamic values.
+
+## How to use
+
+Run code using "npm run dev"
+ Explore with the header menu.
+ Contact page is there to demo what happens when page is not found
---
+++ router-concept/src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Layout from './Layout';
+import Home from './Components/Home';
+import './index.css';
+import User from './Components/User';
+import About from './Components/About';
+import Github ,{githubInfoLoader} from './Components/Github';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route loader ={githubInfoLoader} path="github" element={<Github />} />
+      <Route path="user" element={<User />}>
+        <Route path=":userid" element={<User />} />
+      </Route>
+      <Route path="*" element={<div>Not Found</div>} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
